f9e621b614715d12d7e0159a3975f2c20ea1d3ac
Ryo Masuda <mrra4611@gmail.com>
2018-01-26 18:51:29 +0900

    Add POST /users route and validations for user

--- server/models/user.js
+++ server/models/user.js
@@ -1,18 +1,33 @@
 var mongoose = require('mongoose');
+var validator = require('validator');
 
 var User = mongoose.model('User', {
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 1
-  },
   email: {
     type: String,
     required: true,
     trim: true,
-    minlength: 1
-  }
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      massage: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true,
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
 
 module.exports = {User};
