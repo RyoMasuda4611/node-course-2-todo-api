cd4aae0d98707d1dcef5a4a718cee1e4df25faeb
Ryo Masuda <mrra4611@gmail.com>
2018-01-26 13:00:29 +0900

    Test Delete todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -92,6 +92,43 @@ describe('Get /todos/:id', () => {
       .expect(404)
       .end(done);
   });
-
 });
+
+describe('Delete /todos/:id', () => {
+  it('should remove a todo', (done) => {
+  var hexId = todos[1]._id.toHexString();
+  request(app)
+    .delete(`/todos/${hexId}`)
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todo._id).toBe(hexId);
+    })
+    .end((err, res) => {
+      if(err){
+        return done(err);
+      }
+      Todo.findById(hexId).then((todo) => {
+        expect(todo).toBeFalsy();
+        done();
+      }).catch((e) => done(e));
+    });
+  });
+
+  it('should return 404 if todo is not found', (done) => {
+    var hexId = new ObjectId();
+    request(app)
+    .delete(`/todos/${hexId}`)
+    .expect(404)
+    .end(done);
+  });
+
+  it('should return 404 if id is invalid', (done) => {
+    var invalidId = 123;
+    request(app)
+    .delete(`/todos/${invalidId}`)
+    .expect(404)
+    .end(done);
+  });
+});
+
 
