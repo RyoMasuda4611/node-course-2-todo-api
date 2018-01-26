77e4ad728608a656cf68476c6d2ce59585c4e727
Ryo Masuda <mrra4611@gmail.com>
2018-01-26 11:51:40 +0900

    Add Delete todos/:id route

--- server/server.js
+++ server/server.js
@@ -39,9 +39,24 @@ app.get('/todos/:id', (req, res) => {
     Todo.findById(id).then((todo) => {
       if(todo){
         return res.send({todo});
-      } else {
+      }
+      res.status(404).send();
+    }).catch((e) => {
+      res.status(400).send();
+    });
+  }
+});
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if(!ObjectId.isValid(id)){
+    return res.status(404).send();
+  } else {
+    Todo.findByIdAndRemove(id).then((todo) => {
+      if(!todo){
         return res.status(404).send();
       }
+      res.send({todo});
     }).catch((e) => {
       res.status(400).send();
     });
