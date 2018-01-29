84396a6635987e50923451f1b86da7879144e646
Ryo Masuda <mrra4611@gmail.com>
2018-01-29 16:07:56 +0900

    Hash user password before save

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,9 @@
+
 const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs'); 
 
 var UserShema = new mongoose.Schema({
   email: {
@@ -66,6 +68,20 @@ UserShema.statics.findByToken = function (token) {
     'tokens.access' : 'auth'
   });
 };
+
+UserShema.pre('save', function (next) {
+  var user = this;
+  if(user.isModified('password')){
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
 
 var User = mongoose.model('User', UserShema);
 
